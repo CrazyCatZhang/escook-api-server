6a88f8f877b491c53a5b7cde7505bc97382164ab
Peach Zhang <CrazyCatZhang@outlook.com>
2022-04-01 09:48:32 +0800

    Creat user.js

---
+++ router/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//注册
+router.post('/register', function (req, res) {
+    res.send('register OK');
+});
+
+//登录
+router.get('/login', function (req, res) {
+    res.send('login OK');
+});
+
+module.exports = router;
