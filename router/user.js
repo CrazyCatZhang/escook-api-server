6919c94c61ead9f3f14514fc086923ea66775399
Peach Zhang <CrazyCatZhang@outlook.com>
2022-04-01 16:56:45 +0800

    Import verification middleware to implement verification

--- router/user.js
+++ router/user.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userHandler = require('../router_handler/user');
 
+//导入验证中间件
+const expressJoi = require('@escook/express-joi');
+const {reg_login_schema} = require('../schema/user');
+
 //注册
-router.post('/register', userHandler.register);
+router.post('/register', expressJoi(reg_login_schema), userHandler.register);
 
 //登录
 router.post('/login', userHandler.login);
